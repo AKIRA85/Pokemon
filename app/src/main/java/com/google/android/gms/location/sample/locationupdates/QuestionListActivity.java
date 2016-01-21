9dd854dd3047b600abec2368d8129bbc74307f08
AKIRA85 <akshay.d.mohan@gmail.com>
2016-01-22 03:36:47 +0530

    Question list added to nav drawer and created question_list_item

--- app/src/main/java/com/google/android/gms/location/sample/locationupdates/QuestionListActivity.java
+++ app/src/main/java/com/google/android/gms/location/sample/locationupdates/QuestionListActivity.java
@@ -34,70 +34,20 @@ public class QuestionListActivity extends ListActivity{
         setContentView(R.layout.activity_list);
         mActivity = this;
 
-        String[] queryColumns = {CaptureList._ID,
-                CaptureList.NAME,
-                CaptureList.RANK,
-                CaptureList.LEVEL,
-                CaptureList.TYPE_ID};
-        String[] fromColumns = {CaptureList.NAME,
-                CaptureList.NAME,
-                CaptureList.RANK,
-                CaptureList.LEVEL,
-                CaptureList.TYPE_ID};
-        int[] toViews = {R.id.pokemon_image,
-                R.id.pokemon_name,
-                R.id.pokemon_rank,
-                R.id.pokemon_level,
-                R.id.pokemon_type};
+        String[] queryColumns = {QuestionList._ID,
+                                QuestionList.QNO,
+                                QuestionList.ANSWER,
+                                QuestionList.SCORE};
+        String[] fromColumns = {QuestionList.QNO,
+                QuestionList.ANSWER,
+                QuestionList.SCORE};
 
-        SimpleCursorAdapter.ViewBinder viewBinder = new SimpleCursorAdapter.ViewBinder() {
-            @Override
-            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-                int id = view.getId();
-
-                if(id == R.id.pokemon_image){
-                    String pokemonName = cursor.getString(columnIndex);
-                    Log.i("image", pokemonName);
-                    Drawable drawable = Data.findImageByName(pokemonName, mActivity);
-                    ImageView imageView = (ImageView) view;
-                    imageView.setImageDrawable(drawable);
-                    return true;
-                }else if (id == R.id.pokemon_rank){
-                    TextView textView = (TextView)view;
-                    int rank = cursor.getInt(columnIndex);
-                    textView.setText("Rank : "+rank);
-                    return true;
-                }else if (id == R.id.pokemon_level){
-                    TextView textView = (TextView)view;
-                    int level = cursor.getInt(columnIndex);
-                    textView.setText("Level : "+level);
-                    return true;
-                }else if (id == R.id.pokemon_type){
-                    TextView textView = (TextView)view;
-                    int typeID = cursor.getInt(columnIndex);
-                    String type="Unknown";
-                    switch (typeID){
-                        case 1:type="Fire";
-                            break;
-                        case 2:type="Grass";
-                            break;
-                        case 3:type="Water";
-                            break;
-                        case 4:type="Rock";
-                            break;
-                        case 5:type="Electric";
-                            break;
-                    }
-                    textView.setText("Type : "+type);
-                    return true;
-                }else {
-                    return false;
-                }
-            }
-        };
+        int[] toViews = {R.id.q_no,
+                R.id.q_answer,
+                R.id.q_score};
 
         SQLiteDatabase db = (new PokemonDatabase(this)).getReadableDatabase();
-        Cursor cursor = db.query(CaptureList.TABLE_NAME,
+        Cursor cursor = db.query(QuestionList.TABLE_NAME,
                 queryColumns,
                 null,
                 null,
@@ -108,9 +58,8 @@ public class QuestionListActivity extends ListActivity{
         // Create an empty adapter we will use to display the loaded data.
         // We pass null for the cursor, then update it in onLoadFinished()
         mAdapter = new SimpleCursorAdapter(this,
-                R.layout.simple_list_item, cursor,
+                R.layout.question_list_item, cursor,
                 fromColumns, toViews, 0);
-        mAdapter.setViewBinder(viewBinder);
         setListAdapter(mAdapter);
     }
 
